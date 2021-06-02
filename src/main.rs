be20d7dec443cd1d3f99b5b41b90fc2b85f8f8e1
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-06-02 21:33:11 +0900

    add hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{Result, client};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
 }
